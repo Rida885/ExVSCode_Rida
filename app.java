fa3b5801d8fa7c488fb1ae1e21e84c3cb3213225
Rida885 <ridaregragui73@gmail.com>
2025-12-09 21:05:13 +0100

    mostrar pares

---
+++ app.java
@@ -0,0 +1,9 @@
+public class app {
+    public static void main(String[] args) {
+        System.out.println("dd");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+    
+}
